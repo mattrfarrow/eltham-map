22a3d13f1c726899938c111c3972851a995c10be
Matt Farrow <matt.farrow@unrulygroup.com>
2019-05-13 10:28:59 +0100

    More events

--- events-and-attractions.js
+++ events-and-attractions.js
@@ -5,6 +5,10 @@ var attractions = [
     {name: "Eltham Park South", coords: [0.067033, 51.454722], desc: "Large, grassy area, good for informal ball games with splendid tree-lined avenues, a children's playground and a picnic area." },
     {name: "Severndroog Castle", coords: [0.060037, 51.466584], desc: "An 18th century folly with a café and occasional events." },
     {name: "Tudor Barn", coords: [0.048965, 51.457047], desc: "A restaurant in a 16th century barn." },
+    {name: "Shrewsbury House", coords: [0.072817, 51.473214], desc: "A community centre on Shooters Hill.", website: "https://shrewsburyhouse.org" },
+    {name: "Gerald Moore Gallery", coords: [0.039818, 51.438416], desc: "A young centre for modern and contemporary art", website: "https://geraldmooregallery.org" },
+    {name: "The Rusty Bucket Pub", coords: [0.052095, 51.450422], desc: "The best craft beer in town", website: "https://geraldmooregallery.org" },
+
 ];
 
 var jan = 0;
@@ -27,8 +31,25 @@ var events = [
     {poi: "Severndroog Castle", date: new Date(2019, aug, 11), timeSpecified: 0, title: "A Selection of Shakespeare Plays"},
     {poi: "Severndroog Castle", date: new Date(2019, sep, 13), timeSpecified: 0, title: "A Screening of Hitchcock"},
 
-        {poi: "Tudor Barn", date: new Date(2019, may, 31, 19), timeSpecified: 1, title: "Wine and Dine", shortDesc: "Try wines from around the world"},
+    {poi: "Tudor Barn", date: new Date(2019, may, 31, 19), timeSpecified: 1, title: "Wine and Dine", shortDesc: "Try wines from around the world"},
     {poi: "Tudor Barn", date: new Date(2019, jun, 02, 10), timeSpecified: 1, title: "Family Fun Day", shortDesc: "June family fun day"},
     {poi: "Tudor Barn", date: new Date(2019, jun, 16, 12), timeSpecified: 1, title: "Father's Day"},
     {poi: "Tudor Barn", date: new Date(2019, jul, 22, 11), timeSpecified: 1, title: "Wedding Fair"},
+
+    {poi: "Shrewsbury House", date: new Date(2019, may, 18, 12), timeSpecified: 1, title: "Kid's Crafternoon", shortDesc: "Come along and join us for another Kid’s Crafternoon!  ", url: "https://shrewsburyhouse.org/events/events/kids-crafternoon/"},
+    {poi: "Shrewsbury House", date: new Date(2019, may, 20, 12), timeSpecified: 1, title: "Time Team on Shooters Hill", shortDesc: "Local archaeologist Andy Brockman talks us through the making of a Time Team episode on Shooters Hill.", url: "https://shrewsburyhouse.org/events/events/kids-crafternoon/"},
+    {poi: "Shrewsbury House", date: new Date(2019, may, 31, 19), timeSpecified: 1, title: "Murder Mystery dinner party", shortDesc: "Whodunnit? Can you solve the Mystery? Join us for a fabulous 1930's themed Murder Mystery Dinner party.", url: "https://shrewsburyhouse.org/events/events/kids-crafternoon/"},
+
+    {poi: "Gerald Moore Gallery", date: new Date(2019, may, 23, 18,30), timeSpecified: 1, title: "Introduction to Casting", shortDesc: "Learn some simple casting techniques inspired by our exhibition 'The Oval Window'.", url: "https://bookwhen.com/geraldmooregallery#focus=ev-s34j-20190523183000"},
+    {poi: "Gerald Moore Gallery", date: new Date(2019, jun, 5, 10,00), timeSpecified: 1, title: "Introduction to Drawing", shortDesc: "Come learn to draw in a friendly warm environment.", url: "https://bookwhen.com/geraldmooregallery%20#focus=ev-sg9c-20190501100000"},
+
+    {poi: "Bob Hope Theatre", date: new Date(2019, may, 16, 19,45), timeSpecified: 1, title: "Ruddigore", shortDesc: "A Gilbert & Sullivan play running for three days.", url: "http://www.bobhopetheatre.co.uk/ruddigore.html"},
+    {poi: "Bob Hope Theatre", date: new Date(2019, may, 22, 19,45), timeSpecified: 1, title: "London Suite", shortDesc: "A mixture of comedy and heart-warming drama running until the 25th.", url: "http://www.bobhopetheatre.co.uk/londonsuite.html"},
+    {poi: "Bob Hope Theatre", date: new Date(2019, may, 27, 19,45), timeSpecified: 1, title: "Young Herbert", shortDesc: "A swashbuckling pirate adventure with a twist!", url: "http://www.bobhopetheatre.co.uk/youngherbert.html"},
+    {poi: "Bob Hope Theatre", date: new Date(2019, may, 28, 11,00), timeSpecified: 1, title: "John Hegley: Morning Wordship", shortDesc: "Cross-generational guffaws, comedy word play and song with the legendary performance poet.", url: "http://www.bobhopetheatre.co.uk/hegley.html"},
+
+    {poi: "The Rusty Bucket Pub", date: new Date(2019, may, 19), timeSpecified: 0, title: "Steve Boltz – Live Music (Rock)"},
+    {poi: "The Rusty Bucket Pub", date: new Date(2019, may, 26), timeSpecified: 0, title: "The Mason Dixon Brothers – Live Music (Country/Western)"},
+
+
 ];
