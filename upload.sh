2041580a8f2a85043d2e22ea14103bba1e484382
Matt Farrow <matt.farrow@unrulygroup.com>
2019-05-25 23:28:52 +0100

    feat: clear cache when deploying

--- upload.sh
+++ upload.sh
@@ -1,1 +1,3 @@
 aws s3 cp . s3://inse9-co-uk/ --recursive --exclude "*.md" --exclude "*.sh" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude ".idea/*"
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id E1ENYSQ53KB62P --paths "/*"
