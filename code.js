06864adc684472294be5335d5cc139ffa02ef087
Matt Farrow <matt.farrow@unrulygroup.com>
2019-07-08 21:39:58 +0100

    Full month names as opposed to shortened ones

--- code.js
+++ code.js
@@ -302,7 +302,7 @@ function getTimeAsString(date) {
 
 function dateToString(date) {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return days[date.getDay()] + " " +date.getDate() + " " + months[date.getMonth()];
 }
 
