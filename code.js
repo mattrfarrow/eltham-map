ce72a2ffe2351bb633dbfc75505b4c5f11b9a595
matthewfarrow <mattrfarrow@gmail.com>
2019-11-22 22:20:40 +0000

    extract marker code

--- code.js
+++ code.js
@@ -90,36 +90,7 @@ if(mobile) {
 }
 
 function addMarkers(attraction) {
-    const popupContent = document.createElement("div");
-
-    const popupTitle = document.createElement("h2");
-    popupTitle.innerText = attraction.name;
-    popupContent.appendChild(popupTitle);
-
-    if(attraction.desc) {
-        const popupDesc = document.createElement("div");
-        popupDesc.innerText = attraction.desc;
-        popupContent.appendChild(popupDesc);
-    }
-
-    if(attraction.website) {
-        const popupUrl = document.createElement("a");
-        popupUrl.href = attraction.website;
-        popupUrl.innerText = "Website";
-        popupUrl.target="_blank";
-        popupContent.appendChild(popupUrl);
-    }
-
-    const popup = new mapboxgl.Popup({offset: 25})
-        .setDOMContent(popupContent);
-
-    const el = document.createElement('div');
-    el.className = 'marker';
-
-    new mapboxgl.Marker(el)
-        .setLngLat(attraction.coords)
-        .setPopup(popup) // sets a popup on this marker
-        .addTo(map);
+    buildMarker(attraction).addTo(map);
 }
 
 function showDetailsOfPoiAndEvent(attraction, event) {
